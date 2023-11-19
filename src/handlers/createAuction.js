68b65788dcb8607e3eab6d7f33825c85ff5a63cf
Vladyslav <prostoandrei9@gmail.com>
2023-11-19 22:43:29 +0200

    variables

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,18 +1,33 @@
+import { v4 as uuid } from "uuid";
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, PutCommand } from "@aws-sdk/lib-dynamodb";
+
+const client = new DynamoDBClient();
+const dynamodb = DynamoDBDocumentClient.from(client);
+
 export const createAuction = async (event, context) => {
-    const {title} = JSON.parse(event.body);
+  const { title } = JSON.parse(event.body);
 
-    const createdAt = (new Date()).toISOString();
+  const createdAt = new Date().toISOString();
 
-    const auction = {
-        title,
-        status: "OPEN",
-        createdAt
-    }
-     
-    return {
-        statusCode: 201,
-        body: JSON.stringify(auction)
-    }
+  const auction = {
+    id: uuid(),
+    title,
+    status: "OPEN",
+    createdAt,
+  };
+
+  await dynamodb.send(
+    new PutCommand({
+      TableName: "AuctionsTable",
+      Item: auction,
+    })
+  );
+
+  return {
+    statusCode: 201,
+    body: JSON.stringify(auction),
+  };
 };
 
 export const handler = createAuction;
